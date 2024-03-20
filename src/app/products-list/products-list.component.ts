f000c6a824da7e80dad071cf31c82a4e65390dc8
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-20 20:12:40 +0200

    Create products service with getAll method

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,29 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-
-export interface ProductModel {
-  id: number;
-  name: string;
-  categoryId: number;
-  discount: number;
-  price: number;
-}
-
-export interface ProductResponseModel {
-  products: ProductModel[];
-  limit: number;
-  skip: number;
-  total: number;
-}
-
-const ELEMENT_DATA: ProductModel[] = [
-  { id: 1, name: 'Samsung G50', price: 340.5, discount: 10, categoryId: 1 },
-  { id: 2, name: 'Xiaomi G50', price: 340.5, discount: 15, categoryId: 1 },
-  { id: 3, name: 'Blackberry Curve 8900', price: 70.3, discount: 0, categoryId: 1 }
-];
-
-const api = "https://dummyjson.com/products";
+import { ProductsService } from '../services/products.service';
+import { ProductModel } from '../services/products';
 
 @Component({
   selector: 'app-products-list',
@@ -34,17 +12,11 @@ const api = "https://dummyjson.com/products";
 })
 export class ProductsListComponent implements OnInit {
   displayedColumns: string[] = ["id", "name", "price", "rating"];
-  products: ProductModel[] = ELEMENT_DATA;
+  products: ProductModel[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
-    this.http.get<ProductResponseModel>(api).subscribe(res => {
-      console.log(res.products);
-
-      this.products = res.products;
-    });
+    this.productsService.getAll().subscribe(res => this.products = res.products);
   }
-
-
 }
