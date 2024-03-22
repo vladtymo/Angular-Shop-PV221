e150b86c1e876663e6a953943f145e7134f2a71c
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-22 20:04:30 +0200

    Implement routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponent } from './products/products-list/products-list.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "products", component: ProductsListComponent },
+    { path: "add", component: AddProductComponent }
+];
